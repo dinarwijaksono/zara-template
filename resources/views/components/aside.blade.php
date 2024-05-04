86af96923d9b9e82d96ce5a63e81a5eb9baeba73
dinar wijakosno <dinarwijakosno11@gmail.com>
2024-05-04 20:15:08 +0700

    Desain: desain komponen aside

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -1,16 +1,35 @@
-<aside>
+<aside class="bg-slate-600">
+
+    <div class="profile">
+        <h3 class="text-white mb-0">Dinar wijaksono</h3>
+        <p class="text-[14px] mt-0 text-white">Lorem, ipsum.</p>
+    </div>
+
+    <hr class="my-5 mx-3">
+
     <ul>
         <a href="/">
-            <li>Dashboard</li>
+            <li class="active">
+                Dashboard</li>
         </a>
 
         <a href="/Home/heading">
-            <li>Heading</li>
+            <li>
+                Heading</li>
         </a>
 
         <a href="/Home/colors">
-            <li>Colors</li>
+            <li>
+                Colors</li>
         </a>
-
     </ul>
+
+    <hr class="my-5 mx-3">
+
+    <ul>
+        <li class="btn">
+            <button type="button">Logout</button>
+        </li>
+    </ul>
+
 </aside>
