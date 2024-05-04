f2a691bd137e8cabf2ea778b51f32a16a0b35a70
dinar wijakosno <dinarwijakosno11@gmail.com>
2024-05-04 22:09:00 +0700

    Desain: membuat komponen box

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,8 +1,55 @@
 @extends('./layout/main')
 
 @section('main-section')
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, a! At facilis saepe quos assumenda eaque quis
-        atque error nobis?</p>
+    <h1>Dashboard</h1>
 
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+    <section class="box">
+        <div class="box-title">
+            <h2>Lorem ipsum dolor sit.</h2>
+        </div>
+
+        <div class="box-body">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A architecto omnis numquam aliquid cum delectus
+                debitis blanditiis suscipit soluta sapiente?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero aperiam earum ut impedit consequatur
+                placeat
+                cumque, ea ratione exercitationem rerum inventore itaque recusandae dolorum accusamus tenetur temporibus
+                quis libero molestias!</p>
+        </div>
+    </section>
+
+    <div class="flex gap-2">
+        <div class="basis-1/2">
+            <section class="box ">
+                <div class="box-title">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing</h2>
+                </div>
+
+                <div class="box-body">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis nihil ratione modi voluptas
+                        libero nemo
+                        praesentium iusto dolorem tenetur cum? Porro soluta temporibus modi enim laboriosam, ad labore
+                        dolore
+                        voluptate?</p>
+                </div>
+            </section>
+        </div>
+
+        <div class="basis-1/2">
+            <section class="box ">
+                <div class="box-title">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing</h2>
+                </div>
+
+                <div class="box-body">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis nihil ratione modi voluptas
+                        libero nemo
+                        praesentium iusto dolorem tenetur cum? Porro soluta temporibus modi enim laboriosam, ad labore
+                        dolore
+                        voluptate?</p>
+                </div>
+            </section>
+        </div>
+
+    </div>
 @endsection
