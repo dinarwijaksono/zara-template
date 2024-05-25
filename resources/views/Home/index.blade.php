79c01695a3b1cc8e90ac7af460b4fade1e0666a6
dinar wijakosno <dinarwijakosno11@gmail.com>
2024-05-25 20:33:52 +0700

    Redesain: komponen aside

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -52,4 +52,55 @@
         </div>
 
     </div>
+
+    <section class="box">
+        <div class="box-title">
+            <h2>Lorem ipsum dolor sit.</h2>
+        </div>
+
+        <div class="box-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sint accusantium itaque molestias
+                reiciendis velit neque delectus commodi optio eos sit quidem porro eligendi consequatur debitis alias
+                inventore placeat eius enim minima qui amet laboriosam eaque? Quia recusandae fugit deserunt dolorum a
+                quod officia, consequuntur quaerat aperiam ea nemo nesciunt sit minima voluptatem, sapiente dolor?
+                Placeat labore, inventore ullam ab hic rem quasi quas eligendi porro dolores? Eius repellendus amet vel
+                vitae dolorem facere fuga, ipsam eveniet iusto exercitationem neque, pariatur praesentium a nobis quam
+                culpa aliquam cupiditate laborum nulla sint quas tempora. Delectus laudantium non culpa quo fuga
+                praesentium.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam esse sequi quod nisi corporis non
+                perferendis ipsam libero numquam, nemo odit a nulla, dolor, reiciendis sit fugit incidunt. A
+                reprehenderit iusto odit laboriosam alias repudiandae aliquam, fugiat voluptatibus dicta mollitia.
+                Quibusdam nemo voluptate delectus architecto. Consequuntur quaerat maiores id similique? Quibusdam omnis
+                ullam totam rem cupiditate alias vel voluptatem dicta tenetur. Sequi perspiciatis adipisci voluptates
+                fuga dignissimos quod expedita quae aut, odit accusantium ex quas veritatis, ipsum, provident error non.
+                Cupiditate a ea, nisi omnis consequuntur sapiente ullam, officia, tempora culpa atque laboriosam
+                pariatur explicabo cum ipsum exercitationem excepturi perferendis!</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio totam maiores iure ab corporis natus
+                inventore repudiandae quisquam laboriosam, repellat molestias necessitatibus et ullam exercitationem
+                nesciunt recusandae modi tempore placeat? Provident aliquam qui tempora dolorem quo illum obcaecati
+                quibusdam iure officia perspiciatis maiores, asperiores aperiam minima sint minus, cumque perferendis
+                beatae dicta quia autem eum, impedit quae debitis hic? Esse ipsa aut sint. Sequi perspiciatis tempora,
+                dolore repudiandae error earum aperiam? Minima, deserunt veritatis aperiam architecto, animi error nobis
+                amet, commodi omnis itaque quisquam. Quasi quaerat cupiditate quo quidem voluptates odio corrupti,
+                aperiam quod quis fugit inventore eveniet ipsa dignissimos.</p>
+            <p>Nulla velit non beatae vero voluptatibus minus blanditiis voluptas, tempora eius. Architecto dignissimos
+                incidunt temporibus eveniet reprehenderit repellat porro quisquam nesciunt recusandae iste, et facilis.
+                Voluptatem obcaecati a porro placeat, dolorum hic molestiae quas exercitationem quod perspiciatis
+                tempora quo animi esse deleniti, omnis expedita? Mollitia dolor magnam sint sed sit odio repellat eos
+                doloribus est quasi minima necessitatibus ratione at quisquam omnis voluptas hic nesciunt magni,
+                reiciendis tempora itaque, voluptates provident. Magnam culpa cupiditate sunt ipsam ipsa, aspernatur
+                dolore quos sequi animi vero ullam possimus ex molestias minus iure quis nam, atque nostrum aliquam ad
+                vel laudantium dicta. Ipsa, nam?</p>
+            <p>Tempore, nostrum eius! Voluptas iure rem perferendis libero, et veritatis inventore doloremque quia
+                perspiciatis impedit labore ab sunt ex. Consectetur, omnis! Magnam reprehenderit nihil, a sit saepe
+                odio. Numquam quos soluta blanditiis, fugit mollitia fuga at tempore magnam saepe earum quod sed
+                obcaecati, dolores nulla, explicabo dolorum deleniti quo nostrum nesciunt necessitatibus. Ut explicabo
+                ullam ipsa fugit modi maiores quaerat dolor qui eligendi dolore nobis pariatur consequatur illo
+                dignissimos ducimus soluta molestias minima, aut repellat impedit! Ut quae velit eaque reiciendis quis
+                magnam numquam quasi est unde sapiente dignissimos adipisci similique, modi non dolorum eos ad, officia
+                alias esse suscipit?</p>
+        </div>
+    </section>
 @endsection
